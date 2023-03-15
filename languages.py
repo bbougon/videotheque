65411edeca37ed1a19032207944a2fa002b1b9c8
Bertrand Bougon <bertrandbougon@gmail.com>
2023-03-15 20:18:40 +0100

    Add a progress bar

--- languages.py
+++ languages.py
@@ -11,6 +11,7 @@ _LANGUAGES = {
     "Ita": "ITALIAN",
     "It": "ITALIAN",
     "PORTUGUESE": "PORTUGUESE",
+    "POR": "PORTUGUESE",
     "PT": "PORTUGUESE",
     "Spa": "SPANISH",
     "US": "ENGLISH",
--- search/search_engine.py
+++ search/search_engine.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from typing import List, Optional
 
 from prettytable import PrettyTable
+from progress.bar import IncrementalBar
 
 from infrastructure.ffprobe.ffprobe import FFProbeRunner
 from languages import extract_languages_from_name, map_languages
@@ -58,37 +59,51 @@ class SearchEngine:
         keywords: List[str],
     ) -> SearchResult:
         result = SearchResult()
-        for root, _, files in os.walk(root_path):
-            for name in files:
-                type, _ = mimetypes.guess_type(name)
-                if (
-                    not name.startswith(".")
-                    and type in VIDEO_FILE_TYPES
-                    and self._has_all_keywords_in_name(keywords, name)
-                ):
-                    movie_name, _ = os.path.splitext(Path(name))
-                    details = self.search_for_video_details(
-                        os.path.join(root, name)
-                    )
-                    languages = (
-                        [
-                            language.capitalize()
-                            for language in map_languages(details.languages)
-                        ]
-                        if details.languages
-                        else [
-                            name.capitalize()
-                            for name in extract_languages_from_name(name)
-                        ]
-                    )
-                    result.add(
-                        Movie(
-                            movie_name,
-                            details.duration,
-                            languages,
-                        )
-                    )
+        all_movies = self.__search_all_movies(keywords, root_path)
+        self.__retrieve_movies_informations(all_movies, result)
         return result
+
+    def __retrieve_movies_informations(self, all_movies, result):
+        bar = IncrementalBar(
+            "Searching", max=len(all_movies), color="green", style="faint"
+        )
+        for movie, path in all_movies:
+            details = self.search_for_video_details(path)
+            languages = (
+                [
+                    language.capitalize()
+                    for language in map_languages(details.languages)
+                ]
+                if details.languages
+                else [
+                    name.capitalize()
+                    for name in extract_languages_from_name(movie)
+                ]
+            )
+            result.add(
+                Movie(
+                    movie,
+                    details.duration,
+                    languages,
+                )
+            )
+            bar.next()
+        bar.finish()
+
+    def __search_all_movies(self, keywords, root_path):
+        all_movies = []
+        for root, _, files in os.walk(root_path):
+            type = lambda name: mimetypes.guess_type(name)[0]
+            movie_name = lambda name: os.path.splitext(Path(name))[0]
+            movies = [
+                (movie_name(name), os.path.join(root, name))
+                for name in files
+                if not name.startswith(".")
+                and type(name) in VIDEO_FILE_TYPES
+                and self._has_all_keywords_in_name(keywords, name)
+            ]
+            all_movies.extend(movies)
+        return all_movies
 
     def _has_all_keywords_in_name(
         self, keywords: List[str], name: str
